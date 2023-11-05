5b0f5a65969401890a20c6fc4ee34b5374039cce
mario <maalnu93@hotmail.com>
2023-11-05 13:59:14 +0100

    Llamamos a listProducts en index.js ya vemos los elementos del inventario

---
+++ Scripts/index.js
@@ -0,0 +1,8 @@
+/**
+ * Autor Mario Alfonso Nuñez
+ * GitHub:
+ */
+
+import { productsDisplay } from "./listProducts.js";
+
+document.addEventListener("DOMContentLoaded", productsDisplay);
--- Scripts/listProducts.js
+++ Scripts/listProducts.js
@@ -17,7 +17,7 @@ export const productsDisplay = () => {
         const cell2 = row.insertCell(1)
         const cell3 = row.insertCell(2)
         const cell4 = row.insertCell(3)
-        const cell5 = row.insertCell(4)
+        
 
         cell1.innerHTML = item.titulo
         cell2.innerHTML = item.cantidad

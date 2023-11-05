12de315016cb9743e1afd53049f5756a1993eae7
mario <maalnu93@hotmail.com>
2023-11-05 13:55:41 +0100

    listProducts.js

---
+++ Scripts/listProducts.js
@@ -0,0 +1,28 @@
+/**
+ * Autor Mario Alfonso Nuñez
+ * GitHub:
+ */
+
+import { inventory } from "./products.js";
+
+export const productsDisplay = () => {
+
+    const tableDisplay = document.getElementById("inventory-display")
+
+    inventory.forEach((item) => {
+
+        const row = tableDisplay.insertRow()
+
+        const cell1 = row.insertCell(0)
+        const cell2 = row.insertCell(1)
+        const cell3 = row.insertCell(2)
+        const cell4 = row.insertCell(3)
+        const cell5 = row.insertCell(4)
+
+        cell1.innerHTML = item.titulo
+        cell2.innerHTML = item.cantidad
+        cell3.innerHTML = item.precio
+        cell4.innerHTML = item.formato
+        
+    })
+}
